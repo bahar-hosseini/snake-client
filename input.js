8e7c2c16c01ef2a20ee018ec014be2a48c7134bf
vagrant <bahar.hosseini@gmail.com>
2022-08-03 22:47:53 +0000

    adding the move commands

--- input.js
+++ input.js
@@ -1,16 +1,33 @@
-const setupInput = function() {
+let connection;
+
+const handleUserInput = function(key) {
+  // your code here
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
 
-  const handleUserInput = function(key) {
-  // your code here
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
+  
   
   stdin.on("data", handleUserInput);
   
